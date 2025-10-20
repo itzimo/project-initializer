7081fbbb04fc69c0107d416d3ea24a757ca9bf89
zimo <zimo@itzimo.com>
2025-10-20 11:18:13 +0800

    feat(project): 初始化项目结构和基础配置
    - 添加 .gitignore 文件，排除 IDE 和构建目录
    - 创建 Spring Boot 启动类 Application.java
    - 配置 Maven 多模块项目结构，包含 common、repository、manager、service、web 等模块
    - 引入 Spring Boot 3.0.2 版本依赖管理
    - 配置 Undertow作为内嵌 Web 服务器
    - 添加 Maven 编译插件和版本管理插件 flatten-maven-plugin- 设置 Java 17 编译版本和 UTF-8 编码
    - 创建 BOM 文件统一管理模块版本- 配置各模块间的依赖关系
    - 添加启动模块并配置 spring-boot-maven-plugin 插件

---
+++ project-initializer-stater/src/main/java/com/itzimo/project/initializer/Application.java
@@ -0,0 +1,17 @@
+package com.itzimo.project.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动程序
+ *
+ * @author zimo
+ * @date 2025/10/19
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ project-initializer-stater/src/main/java/com/itzimo/project/initializer/config/UndertowConfiguration.java
@@ -0,0 +1,26 @@
+package com.itzimo.project.initializer.config;
+
+import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Undertow配置
+ *
+ * @author chenmiao
+ * @date 2025/10/19
+ */
+@Configuration
+public class UndertowConfiguration implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+            WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
+            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(true, 8192));
+            deploymentInfo.addServletContextAttribute(
+                    "io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
+        });
+    }
+}
